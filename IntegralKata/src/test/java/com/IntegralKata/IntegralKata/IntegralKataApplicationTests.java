f004c8051802dc4514e4380e53b78a74a99d97e2
spennington45 <spennington45@gmail.com>
2021-02-21 10:31:28 -0600

    Starting Kata with Spring init and resources and dependencies added

---
+++ IntegralKata/src/test/java/com/IntegralKata/IntegralKata/IntegralKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntegralKata.IntegralKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegralKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
